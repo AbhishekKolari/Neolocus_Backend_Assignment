60dde031cd88b8431fec5b1cfe3ee77182ec82c4
unknown <abhishek.kolari@gmail.com>
2024-05-11 20:41:25 +0200

    Client requests script

---
+++ client.py
@@ -0,0 +1,28 @@
+import requests
+import base64
+import json
+# from PIL import Image
+# import io
+
+def process_image(image_path):
+    with open(image_path, 'rb') as f:
+        image_bytes = f.read()
+        image_b64 = base64.b64encode(image_bytes).decode('utf-8')
+
+
+    data = {'image': image_b64}
+    response = requests.post('http://localhost:5000/modify_image', json=data)
+
+    if response.status_code == 200:
+        modified_image_b64 = response.json()['modified_image']
+        modified_image_bytes = base64.b64decode(modified_image_b64)
+        with open('modified_image.jpg', 'wb') as f:
+            f.write(modified_image_bytes)
+        print("Modified image saved as modified_image.jpg")
+    else:
+        print("Error:", response.json()['error'])
+
+# if __name__ == '__main__':
+#     process_image('input_image.png')
+
+process_image('input_image.png')
